1bfc773f937533f7dfe68e7cb1733d3901070836
Can Tonic <can@cantonic.com>
2012-06-15 05:04:10 +0200

    rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PiratenHe::Application.config.secret_token = '3d7a6d8e1910bdf68a5dc9d4e2129ea09da66de0f54a9d0ea7b48d0472e3c93f3007541a1a78ea740accc79dccb49d47c64f2da4ccc4e703cf5356edc01eae81'
